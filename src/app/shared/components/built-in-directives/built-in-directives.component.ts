14d7ea6f9dbc7d16cb77e453841a2b06cc115b03
Minuke <nakamadeath@gmail.com>
2024-10-26 13:34:32 +0200

    add component for angular built-in directives with examples

---
+++ src/app/shared/components/built-in-directives/built-in-directives.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-built-in-directives',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './built-in-directives.component.html',
+  styleUrl: './built-in-directives.component.scss'
+})
+export class BuiltInDirectivesComponent {
+  choose: string = '';
+  color: string = 'green';
+  colorear: string = 'color';
+  presentacion = {
+    color: 'purple',
+    'background-color': '#f4f4f9',
+    'font-weight': 'bold',
+  };
+  items = [
+    { id: 1, name: 'Elemento 1' },
+    { id: 2, name: 'Elemento 2' },
+    { id: 3, name: 'Elemento 3' },
+  ];
+
+  retornarColor(): string {
+    return this.colorear === 'color' ? 'orange' : 'gray';
+  }
+}
