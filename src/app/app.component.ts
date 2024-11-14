8ca8b36b4833082c53ece5b2d9361ec292da66ff
Minuke <nakamadeath@gmail.com>
2024-11-14 23:30:45 +0100

    implement Angular Material tooltips with custom positioning and show/hide delays

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,11 +13,13 @@ import { InputComponent } from '@components/change-detection/input/input.compone
 import { DisplayComponent } from '@components/change-detection/display/display.component';
 import { MockComponent } from '@components/change-detection/mock/mock.component';
 import { EllipsisComponent } from '@components/utilities/ellipsis/ellipsis.component';
+import { TooltipComponent } from '@components/utilities/tooltip/tooltip.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LayoutComponent, BuiltInPipesComponent, CustomPipesComponent, HostListenerComponent, BuiltInDirectivesComponent, CustomDirectivesComponent, BindingsComponent, DeferrableViewsComponent, ContentProjectionComponent, InputComponent, DisplayComponent, MockComponent, EllipsisComponent],
+  imports: [RouterOutlet, LayoutComponent, BuiltInPipesComponent, CustomPipesComponent, HostListenerComponent, BuiltInDirectivesComponent, CustomDirectivesComponent, BindingsComponent, DeferrableViewsComponent, ContentProjectionComponent, InputComponent, DisplayComponent, MockComponent, EllipsisComponent, TooltipComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
