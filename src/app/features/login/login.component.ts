c34eba44df87a5cae777a9b9f92008f2a41f91fe
jminuesa <jminuesa@beateam.es>
2025-02-21 14:44:58 +0100

    Refactor login component to accept username or email, update form validation, and enhance error messaging

--- src/app/features/login/login.component.ts
+++ src/app/features/login/login.component.ts
@@ -12,7 +12,7 @@ export class LoginComponent {
   private readonly formBuilder: FormBuilder = inject(FormBuilder);
 
   public loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
+      userIdentifier: ['', Validators.required],
       password: ['', Validators.required],
   });
 
@@ -20,17 +20,34 @@ export class LoginComponent {
     const control = this.loginForm.get(controlName); 
     if (control?.hasError('required')) { 
       return 'This field is required'; 
+    } else if (control?.hasError('pattern')) {
+      return 'Must be a valid username or email';
     }
     return null;
   }
 
   public onSubmit() { 
     if (this.loginForm.valid) { 
-      console.log('Form submitted successfully', this.loginForm.value); 
+      const { userIdentifier, password } = this.loginForm.value;
+      const payload = this.isEmail(userIdentifier!)
+        ? { email: userIdentifier, password }
+        : { username: userIdentifier, password };
+      console.log('Form submitted successfully', payload); 
     } else { 
       this.loginForm.markAllAsTouched(); 
       console.error('Form is invalid'); 
     }
   }
+
+    /**
+   * Helper function to check if a string is an email or a plain text.
+   * @param input - The string to check.
+   * @returns A boolean indicating if the input is an email (true) or plain text (false).
+   */
+  public isEmail(input: string): boolean {
+    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(input);
+  }
+
 
 }
