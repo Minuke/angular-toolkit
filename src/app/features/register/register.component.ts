bf3a21971ec8db96cc7d9e94bdf95001cd17efd0
jminuesa <jminuesa@beateam.es>
2025-02-24 09:29:54 +0100

    Implement registration component with form validation, error handling, and styling

--- src/app/features/register/register.component.ts
+++ src/app/features/register/register.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
 
+
+    private readonly formBuilder: FormBuilder = inject(FormBuilder);
+  
+    public registerForm = this.formBuilder.group({
+        username: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+    });
+
+    public getErrorMessage(controlName: string): string | null { 
+      const control = this.registerForm.get(controlName); 
+      if (control?.hasError('required')) { 
+        return 'This field is required'; 
+      } else if(control?.hasError('email')) {
+        return 'Must be a valid email';
+      }
+      return null;
+    }
+
+    public onSubmit() { 
+      if (this.registerForm.valid) { 
+        console.log('Form submitted successfully', this.registerForm.value); 
+      } else { 
+        this.registerForm.markAllAsTouched(); 
+        console.error('Form is invalid'); 
+      }
+    }
 }
