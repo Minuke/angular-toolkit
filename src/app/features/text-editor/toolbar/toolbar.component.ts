80481fc966ec9c8a040db431f62d0138003b0e21
jminuesa <jminuesa@beateam.es>
2025-02-25 16:56:08 +0100

    Add toolbar icons for text formatting; implement bold, italic, and underline features

--- src/app/features/text-editor/toolbar/toolbar.component.ts
+++ src/app/features/text-editor/toolbar/toolbar.component.ts
@@ -1,11 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-toolbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.scss'
 })
 export class ToolbarComponent {
+  
+  public activeIcon: string = "";
 
+  public setActive(icon: string): void {
+    this.activeIcon = icon;
+  }
 }
