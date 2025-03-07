aacc83bbcf06ba3fcfe2331712723f158210dce3
Minuke <nakamadeath@gmail.com>
2025-03-07 13:53:23 +0100

    add input and button components with basic templates and styles; set up routing and sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { ButtonComponent } from '@features/button/button.component';
+import { InputComponent } from '@features/input/input.component';
+import { HomeComponent } from '@shared/components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "/home",
+    pathMatch: "full",
+  },
+  {
+    path: "home",
+    component: HomeComponent,
+    title: "Home",
+  },
+  {
+    path: "button",
+    component: ButtonComponent,
+    title: "Button",
+  },
+  {
+    path: "input",
+    component: InputComponent,
+    title: "Input",
+  },
+];
